a927967f54d6b9effc2374d765c9e0e065266228
Gustavo <gustaaavox@gmail.com>
2024-10-19 14:55:16 -0300

    fix: fixed inner.html of the dark-mode button

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
 const firstA = document.querySelector('.header-menu a:first-child')
 
 function changeMode() {
-  const html = document.documentElement
-  html.classList.toggle('light-mode')
+  const body = document.querySelector('body')
+  const bodyClass = body.classList
+  bodyClass.toggle('light-mode')
+
+  if (bodyClass.contains("light-mode")) {
+    firstA.innerHTML = "Modo escuro"
+  }else{
+    firstA.innerHTML = "Modo claro"
+  }
 }
 
 firstA.addEventListener('click', changeMode)
